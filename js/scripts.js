210507050d462bc619e28dd31495e936c1a559fc
Stewart ilondanga <stilondanga@gmail.com>
2019-02-10 16:42:26 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,27 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+var vy = new Vue({
+  el: "#app",
+  data: {
+    l:13,
+    r: 3,
+    q:0,
+    m:0,
+    wholes:0,
+  },
+  methods: {
+    calc: function(){
+      this.q =  this.l/this.r;
+      this.m = this.l%this.r;
+      this.wholes = parseInt(this.q);
+    },
+    decimal: function(){
+      return Number((this.q-this.wholes).toFixed(3));
+    }
+  },
+  mounted: function(){
+    this.calc();
+  }
+});
